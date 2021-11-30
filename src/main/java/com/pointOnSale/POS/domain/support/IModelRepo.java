da85dfb448814ab9c71d55f2db6d7bcfab148693
ayush upadhyay <77016129+AyushAry@users.noreply.github.com>
2021-11-30 15:05:43 +0530

    Update IModelRepo.java

--- src/main/java/com/pointOnSale/POS/domain/support/IModelRepo.java
+++ src/main/java/com/pointOnSale/POS/domain/support/IModelRepo.java
@@ -2,11 +2,7 @@ package com.pointOnSale.POS.domain.support;
 
 import org.springframework.data.jpa.domain.Specification;
 
-/**
- * This interface extends repositories to enable criteria based search
- *
- * @author Kedar (kedar@etasens.com)
- */
+
 public interface IModelRepo {
   public default <T> Specification<T> setSpecification(Specification<T> specification, Specification<T> spec) {
     if (specification == null) {
